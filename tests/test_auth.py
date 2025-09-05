4f0b17c4802b4978866b545816ea10ee1cd0f257
Marzan A <marzan.marss@gmail.com>
2025-09-05 17:57:30 +0300

    init

---
+++ tests/test_auth.py
@@ -0,0 +1,22 @@
+from playwright.sync_api import sync_playwright
+from config.links import Links
+
+def run():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False) 
+        context = browser.new_context()
+        page = context.new_page()
+
+        # Открываем страницу логина Grafana
+        page.goto(f"{Links.HOST}")
+
+        print("👉 Авторизуйся руками в открывшемся браузере, потом вернись в консоль и нажми Enter")
+        input()
+
+        # Сохраняем состояние в state.json
+        context.storage_state(path="state.json")
+
+        browser.close()
+
+if __name__ == "__main__":
+    run()
