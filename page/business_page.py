941cb63dbdf7503b4aa26c1e35c5b61dcbb5bb34
Marzan A <marzan.marss@gmail.com>
2025-09-05 15:13:00 +0300

    init project

---
+++ page/business_page.py
@@ -0,0 +1,13 @@
+import allure
+from base.base_page import BasePage
+from config.links import Links
+
+class BusinessPage(BasePage):
+
+    PAGE_URL = Links.BUSINESS_PAGE
+
+    def __init__(self, page):
+        super().__init__(page)
+        self.button_open_an_account = self.page.get_by_role("link", name="Open an account")
+        self.button_request_the_demo = self.page.get_by_role("button", name="Request the demo")
+        self.button_send_an_email = self.page.get_by_role("link", name="Send an e-mail")
